18836bc9148c4df0f9367501b9aafd4966191f4a
handikacahyo <kontak.handikacahyo@gmail.com>
2020-06-27 17:37:17 +0700

    bug can't display existing api

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-This is learning section to make web based app with React js.
+This is learning section to make covid 19 tracker web app with React js, Material UI, and Chart js.
 
 to start from scratch:
 
 1. Install react :
    <br/> npx create-react-app <folder_name>
 
-2.
+2. Install some package :
+   <br /> npm install --save axios react-chartjs-2 react-countup classnames @material-ui/core
 
-7) Finally, start program & make some web :
+3. Finally, start program & make some web :
    <br/> npm start
+
+API Link : https://covid19.mathdro.id/api
 
 Source : https://www.youtube.com/watch?v=khJlrj3Y6Ls
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 function App() {
+  const [state, setdata] = useState({});
+  const fetch = fetchData();
+
+  useEffect(() => {
+    async function fetchApi() {
+      const fetchData = await fetch;
+
+      setdata({ data: fetchData });
+    }
+    fetchApi();
+  }, []);
+
+  const { data } = state;
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div className={styles.container}>
+      <Cards covidData={data} />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
